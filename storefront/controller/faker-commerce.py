0f1c1131b326ae9909098f3f2ff57ba893925c92
kanacb <kana@cloudbasha.com>
2022-11-23 06:48:36 +0800

    day 3-1

---
+++ storefront/controller/faker-commerce.py
@@ -0,0 +1,9 @@
+#pipenv install faker-commerce
+
+from faker import Faker
+
+import faker_commerce
+
+fake = Faker()
+fake.add_provider(faker_commerce.Provider)
+print(fake.ecommerce_name())  # prints a product name
